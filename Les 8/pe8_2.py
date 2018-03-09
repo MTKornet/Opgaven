8fb7cdceed2acb1735085d83dcb4633c0dfa979c
MTKornet <marald.kornet@student.hu.nl>
2018-03-09 11:35:03 +0100

    Les 8 (14-03-2018)

--- Les 8/pe8_2.py
+++ Les 8/pe8_2.py
@@ -1,23 +1,18 @@
 import random
 def monopolyworp():
+    Teller = 0
     res1=random.randrange(1,7)
     res2=random.randrange(1,7)
-    if res1==res2:
-        print('{} + {} = {} (dubbel)'.format(res1,res2,res1+res2))
-        res1 = random.randrange(1, 7)
-        res2 = random.randrange(1, 7)
-        if res1==res2:
-            print('{} + {} = {} (dubbel)'.format(res1,res2,res1+res2))
+    while res1 == res2:
+        print('{} + {} = {} (dubbel)'.format(res1, res2, res1 + res2))
+        if  Teller < 3:
+            Teller = Teller + 1
             res1 = random.randrange(1, 7)
             res2 = random.randrange(1, 7)
-            if res1==res2:
-                print('{} + {} = (direct naar de gevangenis)'.format(res1,res2))
-            else:
-                 print('{} + {} = {}'.format(res1,res2,res1+res2))
-
         else:
-            print('{} + {} = {}'.format(res1, res2, res1 + res2))
-    else:
-        print('{} + {} = {}'.format(res1, res2, res1 + res2))
+            print('{} + {} = (direct naar de gevangenis)'.format(res1, res2))
+            break
 
-print(monopolyworp())
+    print('{} + {} = {}'.format(res1, res2, res1 + res2))
+
+monopolyworp()
